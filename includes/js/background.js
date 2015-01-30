cca0195198af8a17cdd71c64d0071ddd9f4bfd4b
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2015-01-31 02:22:57 +0600

    Remove debug

--- includes/js/background.js
+++ includes/js/background.js
@@ -1,4 +1,4 @@
-chrome.alarms.create('everyday_alarm', {periodInMinutes: 1});
+chrome.alarms.create('everyday_alarm', {periodInMinutes: 30});
 
 function everyday_notify() {
   chrome.notifications.create(
@@ -21,10 +21,10 @@ chrome.alarms.onAlarm.addListener(function(alarm) {
       var now = new Date();
       var start = new Date(now.getFullYear(), 0, 0);
       var diff = now - start;
-      var oneDay = 1000 * 60;
+      var oneDay = 1000 * 60 * 60 * 24;
       var day = Math.round(diff / oneDay);
 
-      if (items.everyday_last_date == undefined || (items.everyday_last_date > day || day > items.everyday_last_date)) {
+      if (items.everyday_last_date == undefined || items.everyday_last_date != day) {
         chrome.storage.sync.set({'everyday_last_date': day}, function() {});
         chrome.storage.sync.set({'everyday_show': true}, function() {
           everyday_notify();
