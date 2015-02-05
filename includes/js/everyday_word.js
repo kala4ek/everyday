162d370988a7008796938e1d281e14bd67080a0e
Kiselev Dmitry <kiselev.kala4ek@gmail.com>
2015-02-05 08:43:27 +0600

    Add author field

--- includes/js/everyday_word.js
+++ includes/js/everyday_word.js
@@ -21,6 +21,7 @@ var Word = new function() {
           if (response.result.status == 1) {
             jQuery('#word h3.title').html(response.result.data.title_link);
             jQuery('#word p.description').html(response.result.data.description);
+            jQuery('#word div.category').html(chrome.i18n.getMessage('authorTitle') + ': ' + response.result.data.author_link);
 
             chrome.storage.sync.set({'everyday_word_last_data': response.result.data}, function() {});
             chrome.storage.sync.set({'everyday_word_last_id': response.result.data.id}, function() {});
@@ -43,6 +44,7 @@ var Word = new function() {
     chrome.storage.sync.get('everyday_word_last_data', function(items) {
       jQuery('#word h3.title').html(items.everyday_word_last_data.title_link);
       jQuery('#word p.description').html(items.everyday_word_last_data.description);
+      jQuery('#word div.category').html(chrome.i18n.getMessage('authorTitle') + ': ' + items.everyday_word_last_data.author_link);
     });
   }
 };
