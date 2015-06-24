af3a18604f12e6c2018873196797c04ccef99698
Dmitry Kiselev <kiselev.kala4ek@gmail.com>
2015-06-24 22:15:29 +0600

    Remove drupal

--- includes/js/everyday.js
+++ includes/js/everyday.js
@@ -4,12 +4,10 @@ jQuery(document).ready(function() {
   chrome.storage.sync.get('everyday_show', function(items) {
     chrome.browserAction.setBadgeText({text: ''});
     if (items.everyday_show == undefined || items.everyday_show == true) {
-      Drupal.displayData();
       Word.displayData();
       chrome.storage.sync.set({'everyday_show': false}, function() {});
     }
     else {
-      Drupal.displayStoredData();
       Word.displayStoredData();
     }
   });
--- includes/js/everyday_common.js
+++ includes/js/everyday_common.js
@@ -1,28 +1,4 @@
 /**
- * Compare extension versions.
- */
-function version_compare(left, right) {
-  if (typeof left + typeof right != 'stringstring') {
-    return false;
-  }
-
-  var a = left.split('.')
-    ,   b = right.split('.')
-    ,   i = 0, len = Math.max(a.length, b.length);
-
-  for (; i < len; i++) {
-    if ((a[i] && !b[i] && parseInt(a[i]) > 0) || (parseInt(a[i]) > parseInt(b[i]))) {
-      return 1;
-    }
-    else if ((b[i] && !a[i] && parseInt(b[i]) > 0) || (parseInt(a[i]) < parseInt(b[i]))) {
-      return -1;
-    }
-  }
-
-  return 0;
-}
-
-/**
  * Initial of extension.
  */
 function everyday_init() {
@@ -31,6 +7,5 @@ function everyday_init() {
   });
 
   jQuery('p.description').html(chrome.i18n.getMessage('loadingText'));
-  jQuery('li a[aria-controls="drupal"]').html(chrome.i18n.getMessage('drupalBlockTitle'));
   jQuery('li a[aria-controls="word"]').html(chrome.i18n.getMessage('wordBlockTitle'));
 }
